b4e4cb4193ed5e1e725f4332307cf6a8aa12812a
zhangyue <hye213701@126.com>
2017-12-29 15:54:39 +0800

    springboot数据源配置

---
+++ modules/springboot-datasource/src/main/java/com/yue/springboot/datasource/DataSourceApplication.java
@@ -0,0 +1,24 @@
+package com.yue.springboot.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 连接数据库DataSource
+ * <pre>
+ * 	1、配置数据源
+ *  2、多环境配置(profile)
+ *  3、flyway(数据库脚本管理)
+ * </pre>
+ *
+ * @author zhangyue
+ * @since 2017-12-22 14:52:53
+ */
+@SpringBootApplication
+public class DataSourceApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DataSourceApplication.class, args);
+	}
+	
+}
---
+++ modules/springboot-datasource/src/main/resources/db/h2/migration/V1_0_0_2__init.sql
@@ -0,0 +1,1 @@
+insert into user (id, name, password) values (2, 'lisi', '123456');
