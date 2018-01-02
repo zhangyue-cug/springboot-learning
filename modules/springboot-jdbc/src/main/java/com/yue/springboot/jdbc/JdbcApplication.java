392cadbbacaf63afb33e38988f8f5a53755020bf
zhangyue <hye213701@126.com>
2018-01-02 16:11:40 +0800

    修改JdbcApplication注释

--- modules/springboot-jdbc/src/main/java/com/yue/springboot/jdbc/JdbcApplication.java
+++ modules/springboot-jdbc/src/main/java/com/yue/springboot/jdbc/JdbcApplication.java
@@ -4,12 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Description: 连接数据库DataSource
- * <pre>
- * 	1、配置数据源
- *  2、多环境配置(profile)
- *  3、flyway(数据库脚本管理)
- * </pre>
+ * @Description: 使用jdbc方式操作数据库
  *
  * @author zhangyue
  * @since 2017-12-22 14:52:53
